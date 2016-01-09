292831f3b81beb6cf14d6f10e84448b7ef490555
Cédric Hoelzl <cedric.hoelzl@gmail.com>
2016-01-09 14:12:43 +0100

    init

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var http = require('http'),
-    Bot = require('telegram-bot-bootstrap'),
+    Bot = require('telegram-bot-api'),
 	fs = require('fs');//,
 	//botan = require('botanio')('yQgq2Rn02CkBIBf4H580BVW7UWIIx-Wj'),
     //sqlite3 = require('sqlite3').verbose(),
     //db = new sqlite3.Database('hodr');
-	
-	
-var Hodr = new Bot('143487305:AAF4FZRlkyau2AYkuGlvE8ehORDGG7AbmZ8');
-//Hodr.sendMessage("26560517","test");
-//Hodr.getUpdates().then(console.log);
-// ? you'll see an update message. Look for your user_id in "message.from.id"
+ 
+var Hodr = new telegram({
+	token: '143487305:AAF4FZRlkyau2AYkuGlvE8ehORDGG7AbmZ8',
+	updates: {
+		enabled: true
+	}
+});
+ 
+Hodr.on('message', function(message){
+    console.log(message);
+});
+
+
 // USER SETTINGS PART // 
 
 //Your Data
